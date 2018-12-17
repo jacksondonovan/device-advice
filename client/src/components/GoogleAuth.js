b6beb8fb52afe408dddcb55a12745af0a7203ecd
Jackson Donovan <iinventedtwitter@gmail.com>
2018-12-17 12:15:33 -0700

    google auth hooked up, WIP, hooking up auth boolean to reducer

---
+++ client/src/components/GoogleAuth.js
@@ -0,0 +1,63 @@
+import React from 'react';
+
+class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
+
+  componentDidMount() {
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client.init({
+        clientId: '',
+        scope: 'email'
+      }).then(() => {
+        this.auth = window.gapi.auth2.getAuthInstance();
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        this.auth.isSignedIn.listen(this.onAuthChange);
+      })
+    });
+  }
+
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get()});
+  };
+
+  onSignInClick = () => {
+    this.auth.signIn();
+  };
+
+  onSignOutClick = () => {
+    this.auth.signOut();
+  };
+
+
+
+  renderAuthButton() {
+    if(this.state.isSignedIn === null) {
+      return null;
+    } else if(this.state.isSignedIn) {
+      return (
+        <button onClick={this.onSignOutClick} className="ui red google button">
+          <i className="google icon" />
+          Sign Out
+        </button>
+      );
+    } else {
+      return (
+        <button onClick={this.onSignInClick} className="ui red google button">
+          <i className="google icon" />
+          Sign in with Google
+        </button>
+      );
+    }
+  }
+
+
+  render() {
+    return (
+      <div>
+        {this.renderAuthButton()}
+      </div>
+    )
+  }
+}
+
+export default GoogleAuth;
